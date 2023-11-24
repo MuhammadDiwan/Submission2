59f7528980db15d59ca27ad3a58328d103cf03f7
MuhammadDiwan <dianherlina716@gmail.com>
2023-11-24 12:30:28 +0700

    Submission 2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>wellcome to the jungle</h3>
+    <hr>
+    <a href="index">HOME</a> | <a href="about">ABOUT</a> | <a href="contact">CONTACT </a>
+</body>
+</html>
